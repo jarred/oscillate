71e71f8a2572c0e5adc56e89118253144774d914
Jarred <jarred@bitbybit.io>
2012-05-13 12:33:06 +1200

    initial setup and start on audio player

--- _coffee/main.coffee
+++ _coffee/main.coffee
@@ -3,4 +3,20 @@ oscillate.Main =
   init: ->
     SC.initialize
       client_id: "d47b942351e59deb9ec38d90a15beb81"
+
+    oscillate.appModel = new Backbone.Model()
+    
+    @extendViews()
+    return
+
+  extendViews: ->
+    _.each $('.extend'), (el) =>
+      $el = $(el)
+      name = $el.data('view')
+      return if name is null or name is ''
+      return if oscillate.Views[name] is undefined
+      view = new oscillate.Views[name]
+        el: $el
+        appModel: oscillate.appModel
+      $el.removeClass 'extend'
     return
--- js/main.js
+++ js/main.js
@@ -8,6 +8,23 @@
       SC.initialize({
         client_id: "d47b942351e59deb9ec38d90a15beb81"
       });
+      oscillate.appModel = new Backbone.Model();
+      this.extendViews();
+    },
+    extendViews: function() {
+      var _this = this;
+      _.each($('.extend'), function(el) {
+        var $el, name, view;
+        $el = $(el);
+        name = $el.data('view');
+        if (name === null || name === '') return;
+        if (oscillate.Views[name] === void 0) return;
+        view = new oscillate.Views[name]({
+          el: $el,
+          appModel: oscillate.appModel
+        });
+        return $el.removeClass('extend');
+      });
     }
   };
 
